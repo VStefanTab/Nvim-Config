61b19472860390086baed04f6b85826269e75c28
deceneu009 <sstefy8967@gmail.com>
2024-09-07 21:05:40 +0300

    Nvim btw

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,147 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      local dap = require("dap")
+      local ui = require("dapui")
+      local nvim_tree = require("neo-tree")
+
+      require("dapui").setup()
+      require("dap-go").setup()
+
+      require("nvim-dap-virtual-text").setup({
+        display_callback = function(variable)
+          local name = string.lower(variable.name)
+          local value = string.lower(variable.value)
+          if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
+            return "*****"
+          end
+
+          if #variable.value > 15 then
+            return " " .. string.sub(variable.value, 1, 15) .. "... "
+          end
+
+          return " " .. variable.value
+        end,
+      })
+
+      -- Handled by nvim-dap-go
+      -- dap.adapters.go = {
+      --   type = "server",
+      --   port = "${port}",
+      --   executable = {
+      --     command = "dlv",
+      --     args = { "dap", "-l", "127.0.0.1:${port}" },
+      --   },
+      -- }
+
+      local elixir_ls_debugger = vim.fn.exepath("elixir-ls-debugger")
+      if elixir_ls_debugger ~= "" then
+        dap.adapters.mix_task = {
+          type = "executable",
+          command = elixir_ls_debugger,
+        }
+
+        dap.configurations.elixir = {
+          {
+            type = "mix_task",
+            name = "phoenix server",
+            task = "phx.server",
+            request = "launch",
+            projectDir = "${workspaceFolder}",
+            exitAfterTaskReturns = false,
+            debugAutoInterpretAllModules = false,
+          },
+        }
+      end
+
+      -- Add Python Debug Adapter Configuration
+      dap.adapters.python = {
+        type = "server",
+        host = "localhost",
+        port = 5678,
+      }
+
+      dap.configurations.python = {
+        {
+          name = "Launch File",
+          type = "python",
+          request = "launch",
+          program = "${file}",
+          pythonPath = function()
+            return vim.fn.input('Python interpreter path: ', vim.fn.exepath('python3'))
+          end,
+        },
+      }
+
+      -- Configure codelldb for C++
+      local codelldb_path = vim.fn.exepath("codelldb")
+      if codelldb_path ~= "" then
+        dap.adapters.lldb = {
+          type = "server",
+          port = "${port}",
+          executable = {
+            command = codelldb_path,
+            args = { "--port", "${port}" },
+          },
+        }
+
+        dap.configurations.cpp = {
+          {
+            name = "Launch",
+            type = "lldb",
+            request = "launch",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = false,
+            args = {},
+            runInTerminal = true, -- Optional
+          },
+        }
+
+        dap.configurations.c = dap.configurations.cpp -- Use the same config for C
+      end
+
+      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set("n", "<F1>", dap.continue)
+      vim.keymap.set("n", "<F2>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<F13>", dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+        nvim_tree.close()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+        nvim_tree.close()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+        nvim_tree.open()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+        nvim_tree.open()
+      end
+    end,
+  },
+}
