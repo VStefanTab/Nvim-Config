20d87423bbbfbdea89651eaf255a9c0806a0de02
deceneu009 <sstefy8967@gmail.com>
2025-02-20 00:51:27 +0200

    removed the file tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,28 +1,28 @@
-local function close_neo_tree()
-	-- Close neo-tree if it's open
-	vim.cmd("Neotree close")
-end
-
-return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			window = {
-				width = 30,
-				auto_expand_width = false,
-			},
-		})
-
-		-- Keymap to open Neo-tree
-		vim.keymap.set("n", "<C-t>", ":Neotree filesystem reveal left<CR>", {})
-
-		-- Keymap to close Neo-tree
-		vim.keymap.set("n", "<Esc>t", close_neo_tree, {})
-	end,
-}
+--local function close_neo_tree()
+--	-- Close neo-tree if it's open
+--	vim.cmd("Neotree close")
+--end
+--
+--return {
+--	"nvim-neo-tree/neo-tree.nvim",
+--	branch = "v3.x",
+--	dependencies = {
+--		"nvim-lua/plenary.nvim",
+--		"nvim-tree/nvim-web-devicons",
+--		"MunifTanjim/nui.nvim",
+--	},
+--	config = function()
+--		require("neo-tree").setup({
+--			window = {
+--				width = 30,
+--				auto_expand_width = false,
+--			},
+--		})
+--
+--		-- Keymap to open Neo-tree
+--		vim.keymap.set("n", "<C-t>", ":Neotree filesystem reveal left<CR>", {})
+--
+--		-- Keymap to close Neo-tree
+--		vim.keymap.set("n", "<Esc>t", close_neo_tree, {})
+--	end,
+--}
