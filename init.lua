f050dc64589b5a5fdd4d55902715ddf778b85875
deceneu009 <sstefy8967@gmail.com>
2025-03-17 00:23:12 +0200

    help keybinds

--- init.lua
+++ init.lua
@@ -11,8 +11,8 @@ vim.wo.cursorline = true
 -- Limit stuff to 80 chars
 
 vim.wo.wrap = true
-vim.wo.colorcolumn = "80" -- Visual guide at 80 characters
-vim.bo.textwidth = 80 -- Auto-wrap text at 80 characters while typing
+vim.wo.colorcolumn = "80"       -- Visual guide at 80 characters
+vim.bo.textwidth = 80           -- Auto-wrap text at 80 characters while typing
 vim.cmd("set formatoptions+=t") -- Ensure automatic wrapping while typing
 
 -- Leader key
@@ -38,44 +38,54 @@ vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 -- Optional: Set fold level (how deeply the folds should be open by default)
 vim.o.foldlevel = 99
 
+-- Keybind(already defined)
+-- za toggle a fold at the cursor.
+-- zo opens a fold at the cursor.
+-- zO opens all folds at the cursor.
+-- zc closes a fold under cursor.
+-- zm increases the foldlevel by one.
+-- zM closes all open folds.
+-- zr decreases the foldlevel by one.
+-- zR decreases the foldlevel to zero
+
 -- SplitScreen keys
 vim.keymap.set(
-	"n",
-	"<Leader>-",
-	":split<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader>-",
+  ":split<CR>",
+  { noremap = true, silent = true }
 )
 vim.keymap.set(
-	"n",
-	"<Leader>|",
-	":vsplit<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader>|",
+  ":vsplit<CR>",
+  { noremap = true, silent = true }
 )
 
 -- Navigate between splits
 vim.keymap.set(
-	"n",
-	"<Leader>h",
-	":wincmd h<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader>h",
+  ":wincmd h<CR>",
+  { noremap = true, silent = true }
 )
 vim.keymap.set(
-	"n",
-	"<Leader>l",
-	":wincmd l<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader>l",
+  ":wincmd l<CR>",
+  { noremap = true, silent = true }
 )
 vim.keymap.set(
-	"n",
-	"<Leader>j",
-	":wincmd j<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader>j",
+  ":wincmd j<CR>",
+  { noremap = true, silent = true }
 )
 vim.keymap.set(
-	"n",
-	"<Leader>k",
-	":wincmd k<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader>k",
+  ":wincmd k<CR>",
+  { noremap = true, silent = true }
 )
 
 -- Resizing split screens
@@ -84,57 +94,57 @@ vim.keymap.set("n", "<Leader>=", "<C-w>=", { noremap = true, silent = true }) --
 
 -- Resize the current split window
 vim.keymap.set(
-	"n",
-	"<Leader><Up>",
-	":resize +6<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader><Up>",
+  ":resize +6<CR>",
+  { noremap = true, silent = true }
 ) -- Increase height
 vim.keymap.set(
-	"n",
-	"<Leader><Down>",
-	":resize -6<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader><Down>",
+  ":resize -6<CR>",
+  { noremap = true, silent = true }
 ) -- Decrease height
 vim.keymap.set(
-	"n",
-	"<Leader><Left>",
-	":vertical resize -6<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader><Left>",
+  ":vertical resize -6<CR>",
+  { noremap = true, silent = true }
 ) -- Decrease width
 vim.keymap.set(
-	"n",
-	"<Leader><Right>",
-	":vertical resize +6<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader><Right>",
+  ":vertical resize +6<CR>",
+  { noremap = true, silent = true }
 ) -- Increase width
 
 -- Maximizing focused screen
 local is_maximized = false
 
 function ToggleFullscreen()
-	if is_maximized then
-		vim.cmd("wincmd =") -- Restore original layout
-		is_maximized = false
-	else
-		vim.cmd("wincmd |") -- Maximize width
-		vim.cmd("wincmd _") -- Maximize height
-		is_maximized = true
-	end
+  if is_maximized then
+    vim.cmd("wincmd =") -- Restore original layout
+    is_maximized = false
+  else
+    vim.cmd("wincmd |") -- Maximize width
+    vim.cmd("wincmd _") -- Maximize height
+    is_maximized = true
+  end
 end
 
 vim.keymap.set(
-	"n",
-	"<Leader>m",
-	ToggleFullscreen,
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader>m",
+  ToggleFullscreen,
+  { noremap = true, silent = true }
 ) -- Maximize fullscreen
 
 -- Close all other windows except the current one
 vim.keymap.set(
-	"n",
-	"<Leader>a",
-	":wincmd o<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Leader>a",
+  ":wincmd o<CR>",
+  { noremap = true, silent = true }
 ) -- Close others
 
 -- TODO: Make keybinds for tabs
@@ -143,57 +153,57 @@ vim.keymap.set(
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 --local opts = {}
 
 require("lazy").setup({
-	spec = {
-		-- add LazyVim and import its plugins
-		--{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		{ "LazyVim/LazyVim" },
-		--{ import = "lazyvim.plugins.extras.lang.rust" },
-		-- import/override with your plugins
-		{ import = "plugins" },
-	},
-	defaults = {
-		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-		lazy = false,
-		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-		-- have outdated releases, which may break your Neovim install.
-		version = false, -- always use the latest git commit
-		-- version = "*", -- try installing the latest stable version for plugins that support semver
-	},
-	--install = { colorscheme = { "tokyonight", "habamax" } },
-	checker = {
-		enabled = true, -- check for plugin updates periodically
-		notify = true, -- notify on update
-	}, -- automatically check for plugin updates
-	--	  performance = {
-	--	    rtp = {
-	--	      -- disable some rtp plugins
-	--	      disabled_plugins = {
-	--	        "gzip",
-	--	        -- "matchit",
-	--	        -- "matchparen",
-	--	        -- "netrwPlugin",
-	--	        "tarPlugin",
-	--	        "tohtml",
-	--	        "tutor",
-	--	        "zipPlugin",
-	--          "tokyonight",
-	--          "persistance",
-	--	      },
-	--	    },
-	--	  },
+  spec = {
+    -- add LazyVim and import its plugins
+    --{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim" },
+    --{ import = "lazyvim.plugins.extras.lang.rust" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  --install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = true, -- notify on update
+  },              -- automatically check for plugin updates
+  --	  performance = {
+  --	    rtp = {
+  --	      -- disable some rtp plugins
+  --	      disabled_plugins = {
+  --	        "gzip",
+  --	        -- "matchit",
+  --	        -- "matchparen",
+  --	        -- "netrwPlugin",
+  --	        "tarPlugin",
+  --	        "tohtml",
+  --	        "tutor",
+  --	        "zipPlugin",
+  --          "tokyonight",
+  --          "persistance",
+  --	      },
+  --	    },
+  --	  },
 })
