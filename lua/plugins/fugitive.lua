ec4a66b47f0ce28a0092e1474decee68ea090f38
deceneu009 <sstefy8967@gmail.com>
2025-03-16 21:38:31 +0200

    Git in nvim?

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,1 +1,25 @@
-return { "tpope/vim-fugitive.git" }
+return {
+  "git@github.com:tpope/vim-fugitive.git",
+
+  config = function()
+    -- Fugitive keybindings
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>G",
+      ":Git<CR>", -- Use a string command for Gstatus
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>gb",
+      ":Git blame<CR>",
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>gd",
+      ":Git difftool -y<CR>",
+      { noremap = true, silent = true }
+    )
+  end,
+}
